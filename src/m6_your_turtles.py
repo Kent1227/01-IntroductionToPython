a9f22333d39c73e0d76df67898a5dde478343f6d
smithkg <smithkg@rose-hulman.edu>
2018-11-27 15:15:10 -0500

    done

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -32,10 +32,14 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 import rosegraphics as rg
 import math
 
+window = rg.TurtleWindow()
+window.delay(0.5)
+
+
 worden = rg.SimpleTurtle()
 jack = rg.SimpleTurtle()
-worden.pen = rg.Pen('red', 10)
-jack.pen = rg.Pen('blue', 10)
+worden.pen = rg.Pen('red', 1)
+jack.pen = rg.Pen('blue', 3)
 worden.pen_up()
 worden. forward(100)
 worden.left(90)
@@ -44,11 +48,13 @@ worden.left(90)
 worden.pen_down()
 jack.speed = 3
 worden.speed = 4
-for k in range(500):
+for k in range(70):
     worden.forward(200)
-    worden.left(90)
-    a = worden.x_cor()
-    b = worden.y_cor()
-    jack.set_heading((180/3.1415265) * (math.atan(a / b)))
+    worden.left(90 + k)
+    a = worden.x_cor() - jack.x_cor()
+    b = worden.y_cor() - jack.y_cor()
+    print(((180/3.1415265) * (math.atan(a / b))))
+    jack.set_heading((90 * (k % 4)) + math.fabs(((180/3.1415265) * (math.atan(a / b)))))
     jack.forward(k)
+
 
