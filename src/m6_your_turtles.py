e99529f126d90522cc54ca17102cd7a76ae2cc8b
smithkg <smithkg@rose-hulman.edu>
2018-11-27 09:57:31 -0500

    done

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -40,6 +40,15 @@ worden.pen_up()
 worden. forward(100)
 worden.left(90)
 worden.forward(100)
+worden.left(90)
 worden.pen_down()
+jack.speed = 3
+worden.speed = 4
 for k in range(500):
-    jack.set_heading((180/3.1415265) * math.atan(worden.x_cor() / worden.y_cor())) 
+    worden.forward(200)
+    worden.left(90)
+    a = worden.x_cor()
+    b = worden.y_cor()
+    jack.set_heading((180/3.1415265) * (math.atan(a / b)))
+    jack.forward(k)
+
